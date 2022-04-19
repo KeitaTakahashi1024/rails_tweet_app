b92bfe1b7706ae2aad080e905888ab18c42a29fb
KeitaTakahashi1024 <b1017172@fun.ac.jp>
2022-04-19 15:04:58 +0900

    [add]投稿した内容を削除する機能を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get "posts/index" => "posts#index"
-  get "posts/new" => "posts#new"
-  get "posts/:id" => "posts#show"
-
-  post "posts/create" => "posts/create"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#update"
-
-  get "/" => "home#top"
-  get "about" => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
